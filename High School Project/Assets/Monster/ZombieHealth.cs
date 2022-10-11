5d9619d599a4c87c34e5671333428cc76417d388
AggresiveDuck <46778094+Aggressive-Duck@users.noreply.github.com>
2022-10-11 16:06:35 +0800

    BarbedWire

--- High School Project/Assets/Monster/ZombieHealth.cs
+++ High School Project/Assets/Monster/ZombieHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,19 +6,39 @@ using UnityEngine.UI;
 
 public class ZombieHealth : MonoBehaviour
 {
+
     private float hp;
+    float timer = 0;
+    float duration = 0f;
+
     void Start()
     {
         hp = 1.0f;
     }
 
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (timer >= duration && collision.gameObject.tag == "BarbedWire")
+        {
+            duration = 1f;
+            hp -= 0.5f;
+            timer = 0;
+            Debug.Log("Enemy touched");
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "pfBulletProjectile(Clone)")
+        if (collision.gameObject.name == "pfBulletProjectile(Clone)")
         {
             hp -= 0.1f;
         }
-        if(hp <= 0)
+        if (hp <= 0)
         {
             hp = 0f;
             gameObject.tag = "Untagged";
