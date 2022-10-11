0ed493eb788a258d8d446996ef676fae2becbe51
AggresiveDuck <46778094+Aggressive-Duck@users.noreply.github.com>
2022-10-11 13:59:12 +0800

    Change some lights

--- High School Project/Assets/Player/ThirdPersonController.cs
+++ High School Project/Assets/Player/ThirdPersonController.cs
@@ -31,7 +31,7 @@ namespace StarterAssets
 
         public AudioClip LandingAudioClip;
         public AudioClip[] FootstepAudioClips;
-        [Range(0, 1)] public float FootstepAudioVolume = 0.5f;
+        [Range(0, 10)] public float FootstepAudioVolume = 0.5f;
 
         [Space(10)]
         [Tooltip("The height the player can jump")]
