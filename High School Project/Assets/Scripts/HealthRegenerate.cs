dfb35ee7e5a3c4581329088be29c47c244c243bf
AggresiveDuck <46778094+Aggressive-Duck@users.noreply.github.com>
2022-10-25 15:02:11 +0800

    回血加delay

--- High School Project/Assets/Scripts/HealthRegenerate.cs
+++ High School Project/Assets/Scripts/HealthRegenerate.cs
@@ -5,24 +5,29 @@ using UnityEngine;
 public class HealthRegenerate : MonoBehaviour
 {
     float minDist = 2;
-
+    float reChargeTime, currentTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        reChargeTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentTime = Time.time;
         Vector3 playerPosi = GameObject.FindWithTag("Player").transform.position; //用標籤找玩家位置
         Vector3 HealthRegen = GameObject.FindWithTag("Healthregenerate").transform.position;
         GameObject player = GameObject.FindWithTag("Player");
         float dist = Vector3.Distance(playerPosi, HealthRegen);
         if (dist < minDist)
         {
-            // 補血
-            player.gameObject.GetComponent<Health>().TakeDamage(0.0002f, 0.0f);
+            if (currentTime > reChargeTime + 0.6f)
+            {
+                reChargeTime = currentTime;
+                // 補血
+                player.gameObject.GetComponent<Health>().TakeDamage(0.15f, 0.0f);
+            }
         }
     }
 }
