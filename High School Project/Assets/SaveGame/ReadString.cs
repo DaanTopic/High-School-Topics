19baa51d09792816bd36c9ccaf02f936e9f9d7ce
bossliugen <96114403+bossliugen@users.noreply.github.com>
2022-12-18 21:56:52 +0800

    重生點在入成功

--- High School Project/Assets/SaveGame/ReadString.cs
+++ High School Project/Assets/SaveGame/ReadString.cs
@@ -12,44 +12,50 @@ public class ReadString : MonoBehaviour
     public string fileString;
     void Start()
     {
-        GameObject bossi = GameObject.FindWithTag("Boss");
-        GameObject x = GameObject.FindWithTag("rule");
-        using (StreamWriter writer = new StreamWriter(Application.streamingAssetsPath+"Save.txt"))
-        {
-            // Write the string to the file
-            writer.Write(1 +","+ 1 +","+ 1 +","+x.GetComponent<gamerules>().schedule+","+x.GetComponent<gamerules>().killamount+","+bossi.GetComponent<Boss>().i);
+        // Vector3 playerPosi = GameObject.FindWithTag("Player").transform.position; 
+        // GameObject bossi = GameObject.FindWithTag("Boss");
+        // GameObject x = GameObject.FindWithTag("rule");
+        // using (StreamWriter writer = new StreamWriter(Application.streamingAssetsPath+"Save.txt"))
+        // {
+        //     // Write the string to the file
+        //     writer.Write(1 +","+ 1 +","+ 1 +","+x.GetComponent<gamerules>().schedule+","+x.GetComponent<gamerules>().killamount+","+bossi.GetComponent<Boss>().i);
             
-        }
-        using (StreamReader reader = new StreamReader(Application.streamingAssetsPath+ "Save.txt"))
-        {
-            // Read the entire file and store the string in a variable
-            fileString = reader.ReadToEnd();
+        // }
+        // using (StreamReader reader = new StreamReader(Application.streamingAssetsPath+ "Save.txt"))
+        // {
+        //     // Read the entire file and store the string in a variable
+        //     fileString = reader.ReadToEnd();
 
-        }
+        // }
     }
     
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 playerPosi = GameObject.FindWithTag("Player").transform.position; 
         GameObject bossi = GameObject.FindWithTag("Boss");
         GameObject x = GameObject.FindWithTag("rule");
         TMP=fileString.Split(',');
         //Debug.Log("MISSION"+TMP[3]+TMP[4]);
-
+        if (Input.GetKey(KeyCode.P))
+        {
+            ContinueLoading();
+        }
         
         if(x.GetComponent<gamerules>().schedule==6 || x.GetComponent<gamerules>().schedule==10 || x.GetComponent<gamerules>().schedule==13 || x.GetComponent<gamerules>().schedule==16){
-            Debug.Log("------------------------------------------------"+TMP[3]+TMP[4]);
+            Debug.Log(playerPosi[0]+"------------------------------------------------"+TMP[3]+TMP[4]);
             SaveMission();
             
         }
     }
     void SaveMission(){
+        Vector3 playerPosi = GameObject.FindWithTag("Player").transform.position; 
         GameObject bossi = GameObject.FindWithTag("Boss");
         GameObject x = GameObject.FindWithTag("rule");
         using (StreamWriter writer = new StreamWriter(Application.streamingAssetsPath+"Save.txt"))
             {
-                writer.Write(1 +","+ 1 +","+ 1 +","+x.GetComponent<gamerules>().schedule+","+x.GetComponent<gamerules>().killamount+","+bossi.GetComponent<Boss>().i);
+                writer.Write(playerPosi[0] +","+ playerPosi[1] +","+ playerPosi[2] +","+x.GetComponent<gamerules>().schedule+","+x.GetComponent<gamerules>().killamount+","+bossi.GetComponent<Boss>().i);
 
             }
         Loading();
@@ -64,5 +70,28 @@ public class ReadString : MonoBehaviour
             fileString = reader.ReadToEnd();
 
         }
+    }
+    void ContinueLoading(){
+        Vector3 playerPosi = GameObject.FindWithTag("Player").transform.position; 
+        GameObject bossi = GameObject.FindWithTag("Boss");
+        GameObject x = GameObject.FindWithTag("rule");
+        using (StreamReader reader = new StreamReader(Application.streamingAssetsPath+ "Save.txt"))
+        {
+            // Read the entire file and store the string in a variable
+            fileString = reader.ReadToEnd();
+
+        }
+        Vector3 setplayer = new Vector3(float.Parse(TMP[0]), float.Parse(TMP[1]), float.Parse(TMP[2]));
+        GameObject.FindWithTag("Player").transform.SetPositionAndRotation(setplayer,transform.rotation);
+        x.GetComponent<gamerules>().schedule=x.GetComponent<gamerules>().Mission(int.Parse(TMP[3])-1);
+        x.GetComponent<gamerules>().killamount=int.Parse(TMP[4]);
+        if(int.Parse(TMP[3]) >= 13){
+            x.GetComponent<gamerules>().keyget=true;
+            Destroy(GameObject.Find("Key"));
+        }
+        if(int.Parse(TMP[3]) >= 16){
+            x.GetComponent<gamerules>().BossPostEventDialogue=true;
+        }
+        bossi.GetComponent<Boss>().i=int.Parse(TMP[5]);
     }
 }
